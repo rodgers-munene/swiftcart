04e79d44af302adaafe804e8441e8e4b0dedc236
rodgers-munene <munenerodgers72@gmail.com>
2025-06-01 11:52:22 +0300

    added fashion and accesories section and the only this week offer section

---
+++ frontend/src/components/flexProducts.jsx
@@ -0,0 +1,72 @@
+import { ArrowRight } from 'lucide-react'
+import React from 'react'
+
+const FlexProducts = () => {
+  return (
+    <div className='flex overflow-x-auto overflow-y-hidden w-screen sm:w-[95vw] p-2 justify-between'>
+        {/* groceries */}
+        <div className='flex bg-white dark:bg-gray-950 rounded-lg min-w-[20rem] sm:min-w-[26rem] h-44 sm:h-52 relative mr-2'>
+            <div className=' flex flex-col justify-around pl-2 items-start'>
+                <p className='text-amber-500'>Only this week</p>
+                <h1 className='sm:text-xl font-bold w-2/3'>We make your grocery shopping more exciting</h1>
+                <p className='text-gray-500'>A family place for grocery</p>
+                <button className='flex items-center bg-gray-100 dark:bg-gray-900 py-2 px-2 rounded-lg'>Shop Now <ArrowRight className='w-4 h-4'/></button>
+            </div>
+            <div className='absolute  -right-6 sm:right-0 '>
+                <div className='relative overflow-hidden top-4'>
+                <img className='w-44 h-44 absolute -top-12 right-6'
+                src="https://cdn.dummyjson.com/product-images/groceries/apple/thumbnail.webp" alt="apples" />
+                <img className='w-44 h-44'
+                src="https://cdn.dummyjson.com/product-images/groceries/cucumber/thumbnail.webp" alt="cucumber" />
+                </div>
+            </div>
+        </div>
+
+        {/* Electronics */}
+        <div className='flex bg-white dark:bg-gray-950 rounded-lg min-w-[20rem] sm:min-w-[26rem] h-44 sm:h-52 relative overflow-hidden mr-2'>
+            <div className=' flex flex-col justify-around pl-2 items-start'>
+                <p className='text-amber-500'>Only this week</p>
+                <h1 className='sm:text-xl font-bold '>We provide you the best quality products</h1>
+                <p className='text-gray-500'>Smart living made simple</p>
+                <button className='flex items-center bg-gray-100 dark:bg-gray-900 py-2 px-2 rounded-lg'>Shop Now <ArrowRight className='w-4 h-4'/></button>
+            </div>
+            <div className='relative w-[60%] '>
+                
+                <img className='w-32 h-32 absolute top-0 '
+                src="https://cdn.dummyjson.com/product-images/laptops/apple-macbook-pro-14-inch-space-grey/thumbnail.webp" alt="macbook" />
+                
+               <img className='w-28 h-28 absolute bottom-0'
+                src="https://cdn.dummyjson.com/product-images/smartphones/iphone-13-pro/thumbnail.webp" alt="iphone-13" />
+
+                <img className=' w-28 h-28 absolute bottom-0 right-0'
+                src="https://cdn.dummyjson.com/product-images/mobile-accessories/apple-airpods-max-silver/thumbnail.webp" alt="airpod-max" />
+            </div>
+        </div>
+
+        {/* furniture and home deco */}
+        <div className='flex bg-white dark:bg-gray-950 rounded-lg min-w-[20rem] sm:min-w-[26rem] h-44 sm:h-52 relative overflow-hidden'>
+            <div className=' flex flex-col justify-around pl-2 items-start'>
+                <p className='text-amber-500'>Only this week</p>
+                <h1 className='sm:text-xl font-bold '>Style and comfort, made simple.</h1>
+                <p className='text-gray-500'>Comfort and style for every home.</p>
+                <button className='flex items-center bg-gray-100 dark:bg-gray-900 py-2 px-2 rounded-lg'>Shop Now <ArrowRight className='w-4 h-4'/></button>
+            </div>
+            <div className='relative w-[60%] '>
+                
+                <img className='w-32 h-32 absolute top-0 '
+                src="https://cdn.dummyjson.com/product-images/furniture/annibale-colombo-bed/thumbnail.webp" alt="bed" />
+                
+               <img className='w-28 h-28 absolute bottom-0'
+                src="https://cdn.dummyjson.com/product-images/furniture/bedside-table-african-cherry/thumbnail.webp" alt="cabinet" />
+
+                <img className=' w-28 h-28 absolute bottom-0 right-0'
+                src="https://cdn.dummyjson.com/product-images/furniture/knoll-saarinen-executive-conference-chair/thumbnail.webp" alt="chair" />
+            </div>
+        </div>
+
+    </div>
+  )
+}
+
+export default FlexProducts
+
--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -2,15 +2,19 @@ import { useEffect, useState } from 'react'
 import GridProducts from '../components/gridProducts'
 import { topCategories, getProductsInCategory } from '../services/backendApi'
 import { ArrowRight } from 'lucide-react'
+import FlexProducts from '../components/flexProducts'
 
 const Home = () => {
-  const [beautyProducts, setBeautyProducts] = useState()
+  const [beautyProducts, setBeautyProducts] = useState([]);
+  const [fashionProducts, setFashionProducts] = useState([])
+
 
   useEffect(() => {
     const getData = async () => {
       const beautyData = await getProductsInCategory(['beauty', 'skin-care'], 8)
-      console.log(beautyData)
+      const fashionData = await getProductsInCategory(['mens-shirts', 'womens-dresses'], 4)
       setBeautyProducts(beautyData)
+      setFashionProducts(fashionData)
     }
 
     getData()
@@ -34,12 +38,27 @@ const Home = () => {
       </div>
 
       <div className='w-full min-h-screen bg-gray-300 dark:bg-gray-700 flex items-center flex-col'>
+        {/* beauty section */}
         <div className='flex items-center flex-col mt-5'>
-          <div className=' w-full flex justify-between mb-5'>
+
+          <div className=' w-[95vw] flex justify-between mb-5 items-center'>
             <p className='text-sm sm:text-base lg:text-xl font-bold'>Beauty and Skincare</p>
             <button className='flex rounded-md text-sm px-2 py-1 text-black bg-white items-center'>View all <ArrowRight className='w-4 h-4'/></button>
           </div>
           <GridProducts items={beautyProducts}/>
+        </div>
+        {/* only this week section */}
+        <div className='my-5'>
+          <FlexProducts />
+        </div>
+
+        {/* fashion section */}
+        <div className='flex items-center flex-col mt-5 w-full mb-5'>
+          <div className=' w-[95vw] flex justify-between mb-5 items-center'>
+            <p className='text-sm sm:text-base lg:text-xl font-bold'>Fashion and Accessories</p>
+            <button className='flex rounded-md text-sm px-2 py-1 text-black bg-white items-center'>View all <ArrowRight className='w-4 h-4'/></button>
+          </div>
+          <GridProducts items={fashionProducts} />
         </div>
       </div>
 
