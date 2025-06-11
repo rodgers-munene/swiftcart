5445f8e83dafb52f5d2ca45387c0db07daab6d77
rodgers-munene <munenerodgers72@gmail.com>
2025-06-11 22:42:48 +0300

    update mpesa callback handler

--- backend/mpesa/callbackHandler.js
+++ backend/mpesa/callbackHandler.js
@@ -1,13 +1,37 @@
 const Order = require('../models/orderModel')
 
 const handleCallback = (req, res) => {
-    console.log('🔔 Callback endpoint was hit');
-    const data = req.body;
-    console.log('Callback recieved:', JSON.stringify(data, null, 2))
+    let status = req.body.Body.stkCallback.ResultCode;
+  console.log(status);
+  //check if payment was successful or not
+  if (status <= 0) {
+    let message = {
+      ResponseCode: status,
+      ResponseDesc: "success",
+    };
+    //connect to the db
+    //storing the payers details from the callBack
+    const paidDetails = req.body.Body.stkCallback.CallbackMetadata.Item;
 
-    // update the order db after callback
+    const mpesaTransactions = {
+      payingPhoneNumber : paidDetails[4].Value,
+      transationDate:paidDetails[3].Value,
+      mpesaReceiptNumber:paidDetails[1].Value,
+      paidAmount:paidDetails[0].Value,
+      merchantRequestID:req.body.Body.stkCallback.MerchantRequestID,
+      checkoutRequestID:req.body.Body.stkCallback.CheckoutRequestID
+    };
+    //response to safaricom message
+    res.send(message);
 
-    res.status(200).json({});
+  } else {
+    let message = {
+      ResponseCode: status,
+      ResponseDesc: "fail",
+    };
+    //response to safaricom message
+    res.send(message);
+  }
 }
 
 module.exports = { handleCallback }
